a3ea5733c2a3d46aa8e3165ab962f74ae205b869
Isaiah Ernest Ovie <officialisaiahovie@gmail.com>
2022-09-06 23:07:36 +0100

    Fix

--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -1,29 +1,44 @@
 import { onAuthStateChanged } from 'firebase/auth';
 import { doc, getDoc } from 'firebase/firestore';
-import { Navbar } from '../../components';
+import { useCallback, useEffect, useState } from 'react';
+import { BusyIndicator, Navbar } from '../../components';
 import useUserContext from '../../context/userContext';
 import { auth, db } from '../../services/firebase.config';
 import useAuth from '../../services/useAuth';
 
 const Home = () => {
   useAuth();
-  const { setUsername, setReferralLink } = useUserContext();
+  const { setUsername, setReferralLink, username } = useUserContext();
+  const [isLoading, setIsLoading] = useState(false);
 
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      const docRef = doc(db, 'users', user.uid);
-      getDoc(docRef).then((snapshot) => {
-        setUsername(() => snapshot.data().username);
-        setReferralLink(() => snapshot.data().referral_link);
-      });
-    }
-  });
+  const getUserData = useCallback(() => {
+    setIsLoading(true);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const docRef = doc(db, 'users', user.uid);
+        getDoc(docRef).then((snapshot) => {
+          setUsername(() => snapshot.data().username);
+          setReferralLink(() => snapshot.data().referral_link);
+          setIsLoading(false);
+        });
+      }
+    });
+  }, [username]);
+
+  useEffect(() => {
+    getUserData();
+  }, []);
 
   return (
     <>
       <Navbar />
       <section className='page'>
-        <div>Welcome, {}</div>
+        {isLoading && <BusyIndicator />}
+        {!isLoading && (
+          <>
+            <div>Welcome, {username}</div>
+          </>
+        )}
       </section>
     </>
   );
