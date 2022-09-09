2ecb28f8fdb3d2760e36a0b6641cf18c10b1c38c
Isaiah Ernest Ovie <officialisaiahovie@gmail.com>
2022-09-09 20:14:12 +0100

    Fix missing dependencies

--- src/pages/user/Message.jsx
+++ src/pages/user/Message.jsx
@@ -42,11 +42,20 @@ const Message = () => {
     <section className='overflow-hidden h-screen bg-gray-100 py-8'>
       {isLoading && <BusyIndicator />}
       {!isLoading && (
-        <div className='max-w-[360px] m-auto py-8 bg-white p-8 border-2 border-gray-800'>
-          <h2 className='font-black mb-2 text-xl text-purple-700'>Message: </h2>
-          <h2 className='font-bold text-2xl'>{message.text}</h2>
-          <div className='h-[2px] bg-purple-700 w-full my-4'></div>
-          <button className='uppercase text-center py-1 w-full rounded-lg bg-gray-900 block text-white font-bold'>
+        <div className='max-w-[360px] m-auto'>
+          <div
+            className='w-full m-auto py-8 bg-transparent p-8 border-2 border-gray-800 rounded-xl'
+            style={{
+              background: 'linear-gradient(red, green, blue)',
+            }}
+          >
+            <h2 className='font-black mb-2 text-xl text-purple-700'>
+              Message:{' '}
+            </h2>
+            <h2 className='font-bold text-2xl'>{message.text}</h2>
+            <div className='h-[2px] bg-purple-700 w-full my-4'></div>
+          </div>
+          <button className='uppercase text-center py-1 w-full rounded-lg border-gray-900 border-2 block font-bold my-8'>
             Save as image
           </button>
           <h1 className='text-xl text-purple-700 font-bold my-4 uppercase'>
